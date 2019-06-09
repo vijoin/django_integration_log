53591babc6076375de18c2797945331e62c48af2
Victor Inojosa <vijoin@gmail.com>
2019-06-09 12:15:51 -0300

    Add model Integration Lines

--- integration/admin.py
+++ integration/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from integration.models import Integration
+from integration.models import Integration, IntegrationLines
 
 
 @admin.register(Integration)
 class IntegrationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(IntegrationLines)
+class IntegrationLines(admin.ModelAdmin):
     pass
 
 
--- integration/models.py
+++ integration/models.py
@@ -14,3 +14,21 @@ class Integration(models.Model):
                                         self.user_id,
                                         self.date,
                                         )
+
+class IntegrationLines(models.Model):
+    """Integration Details"""
+
+    module_id = models.CharField(max_length=100)  #m2o hacia integration.module
+    repository_id = models.CharField(max_length=100)  #m2o hacia integration.repository
+    server_id = models.CharField(max_length=100)  #m2o hacia integration.server
+    db_id = models.CharField(max_length=100)  #m2o hacia integration.server.bd
+    description = models.TextField(max_length=100)
+    state = models.CharField(max_length=100,
+                             choices=[('draft', 'Draft'),
+                                      ('testing', 'Testing'),
+                                      ('confirmed', 'Confirmed'),
+                                      ('production', 'Production')])
+    validator_ids = models.CharField(max_length=100)  #m2m to users
+
+    def __str__(self):
+        return "{} - {}".format(self.module_id, self.state)
